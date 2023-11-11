e9ada95be8a3792283f06675072df89ac1a2a385
Xavier Hernandez Reyes <xavier.hernandez@capgemini.com>
2023-11-11 12:35:31 -0600

    Se agregan validaciones para ejemplos

--- TypeScript/ejercicios/3.2 - JS vs TypeScript/js-only.js
+++ TypeScript/ejercicios/3.2 - JS vs TypeScript/js-only.js
@@ -1,11 +1,11 @@
-const btnSum = document.getElementById("btnSum");
-const txtNum1 = document.getElementById("txtNum1");
-const txtNum2 = document.getElementById("txtNum2");
+// const btnSum = document.querySelector("btnSum");
+// const txtNum1 = document.getElementById("txtNum1");
+// const txtNum2 = document.getElementById("txtNum2");
 
-function sum(num1, num2) {
-    return num1 + num2;    
-}
+// function sum(num1, num2) {
+//     return num1 + num2;    
+// }
 
-btnSum.addEventListener("click", function() {
-    console.log(sum(txtNum1.value, txtNum2.value));
-});
+// btnSum.addEventListener("click", function() {
+//     console.log(sum(txtNum1.value, txtNum2.value));
+// });a
--- TypeScript/ejercicios/3.2 - JS vs TypeScript/typescript-only.ts
+++ TypeScript/ejercicios/3.2 - JS vs TypeScript/typescript-only.ts
@@ -1,11 +1,13 @@
-// const btnSum = document.getElementById("btnSum");
-// const txtNum1 = document.getElementById("txtNum1");
-// const txtNum2 = document.getElementById("txtNum2");
+const btnSum = document.getElementById("btnSum")! as HTMLButtonElement;
+const txtNum1 = document.getElementById("txtNum1")! as HTMLInputElement;
+const txtNum2 = document.getElementById("txtNum2")! as HTMLInputElement;
 
-// function sumar(num1, num2) {
-//     return num1 + num2;    
-// }
+function sumar(num1, num2) {
+    return num1 + num2;    
+}
 
-// btnSum.addEventListener("click", function() {
-//     console.log(sumar(txtNum1.value, txtNum2.value));
-// });
+btnSum.addEventListener("click", function() {
+    // console.log(sumar(+txtNum1.value, +txtNum2.value));
+    console.log(sumar(parseInt(txtNum1.value), parseInt(txtNum2.value)));
+    // console.log(sumar(Number(txtNum1.value), Number(txtNum2.value)))
+});
